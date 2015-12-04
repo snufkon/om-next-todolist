93c6830841c455696f883bc2dcbfd3be1de9e1d1
snufkon <snufkon@gmail.com>
2015-12-04 22:39:59 +0900

    enable to display todo list

--- src/om_next_todolist/core.cljs
+++ src/om_next_todolist/core.cljs
@@ -5,4 +5,31 @@
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def app-state
+  (atom {:todos [{:id 1 :title "豚肉を買ってくる"}
+                 {:id 2 :title "たまねぎを買ってくる"}
+                 {:id 3 :title "にんじんを買ってくる"}
+                 {:id 4 :title "じゃがいもを買ってくる"}
+                 {:id 5 :title "カレーを作る"}]}))
+
+(defui TodoItem
+  Object
+  (render [this]
+    (let [props (om/props this)
+          title (:title props)]
+      (dom/li nil title))))
+
+(def todo-item (om/factory TodoItem))
+
+(defui TodoList
+  Object
+  (render [this]
+    (let [props (om/props this)
+          todos (:todos props)]
+      (apply dom/ul nil
+        (map todo-item todos)))))
+
+(def reconciler
+  (om/reconciler {:state app-state}))
+
+(om/add-root! reconciler TodoList (gdom/getElement "app"))
