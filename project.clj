7c2cd0fff0c12ebffecf078b5de886fd2f4cb7c3
snufkon <snufkon@gmail.com>
2016-06-10 16:21:38 +0900

    bump up version number

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject om-next-todolist "0.1.0-SNAPSHOT"
   :description "ToDo List example using om.next"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.189"]
-                 [org.omcljs/om "1.0.0-alpha24"]
-                 [figwheel-sidecar "0.5.0-2" :scope "test"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.36"]
+                 [org.omcljs/om "1.0.0-alpha36"]
+                 [figwheel-sidecar "0.5.3-2" :scope "test"]])
